9fac983682d4f914ebff35cb8c52c08a4ad72569
Matt Brown <matt@mattb.net.nz>
2016-06-29 00:43:56 +1200

    Fix current cut-out to be motor shield dependent.
    The Arduino and Pololu motor shields have very different current sense ratios
    (1.65V/A vs 0.525V/A respectively), this means that the Pololu board
    effectively gets no current protection from the sketch with the current
    settings. That might be OK because the Pololu has built-in short-circuit
    protection that generally acts faster than the Arduino's sampling can at
    current rates, however I feel that fixing this is the right thing to do anyway.
    
    Also added documentation to describe how the numbers are arrived at.

--- DCCpp_Uno/CurrentMonitor.h
+++ DCCpp_Uno/CurrentMonitor.h
@@ -13,7 +13,17 @@ Part of DCC++ BASE STATION for the Arduino
 #include "Arduino.h"
 
 #define  CURRENT_SAMPLE_SMOOTHING   0.01
-#define  CURRENT_SAMPLE_MAX         300
+// This is the raw value sampled by analogRead into 1024 steps from 5V
+// aka 0.0049V per step. We need different thresholds per motor driver
+// as they have different V/A ratios on their current sense pins.
+#if MOTOR_SHIELD_TYPE == 0
+  // Arduino Motor Shield, 1.65V/A.
+  #define  CURRENT_SAMPLE_MAX         300     // 890mA == 300*0.0049/1.65
+#elif MOTOR_SHIELD_TYPE == 1
+  #define  CURRENT_SAMPLE_MAX          95     // 890mA == 95*0.0049/0.525
+#else
+  #error CANNOT COMPILE - PLEASE SELECT A PROPER MOTOR SHIELD TYPE
+#endif
 
 #ifdef ARDUINO_AVR_UNO                        // Configuration for UNO
   #define  CURRENT_SAMPLE_TIME        10
